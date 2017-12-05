48a21a54a0236bf45d7131d7968ccfdff02677fc
Johan Burell <johan.burell@valtech.se>
2017-12-05 03:49:57 +0100

    Working on the select device menu. Still not very nice code, but I'm getteing there. Two unwraps remaining.

--- examples/read_events.rs
+++ examples/read_events.rs
@@ -1,18 +1,49 @@
+#![feature(io)]
+#![feature(exclusive_range_pattern)]
 extern crate event_parse;
 extern crate itertools;
 use itertools::Itertools;
+use std::process;
+use std::io::{Read, Write};
 
 fn main() {
 	match event_parse::list_devices() {
 		Ok(devices) => {
+			let num_devices = devices.len() as u32;
 			devices.into_iter()
 			.sorted_by(|a, b| Ord::cmp(&a.0, &b.0))
 			.into_iter()
 			.for_each(|device| {
 				let dev = (device.1).0; // Ewwww....
 				println!("{}: {:?}", device.0, dev);
-			})
+			});
+
+			print!("\nPick a device: ");
+			std::io::stdout().flush().unwrap();
+
+			let c = std::io::stdin()
+				.chars()
+				.next()
+				.and_then(|result| result.ok()).unwrap();
+			
+			if let Some(number) = c.to_digit(10) {
+				match number {
+					number if number > 0 && number < num_devices => {
+						println!("You picked: {}", number);
+					},
+					_ => {
+						println!("Please choose a valid device number between 0 and {}!", num_devices);
+						process::exit(1);
+					},
+				}
+			} else {
+				println!("Please choose a valid device number between 0 and {}!", num_devices);
+				process::exit(1);
+			}
 		},
-		Err(e) => println!("Could not access device list: {}", e),
+		Err(e) => {
+			println!("Could not access device list: {}", e);
+			process::exit(1);
+		},
 	}
 }
